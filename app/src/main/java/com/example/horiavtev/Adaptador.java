2ce8066986984af5ec3aac3ae3fddea66b1b109b
jackdnr <jwrublak@gmail.com>
2023-03-28 22:05:32 -0300

    Initial commit

---
+++ app/src/main/java/com/example/horiavtev/Adaptador.java
@@ -0,0 +1,28 @@
+package com.example.horiavtev;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class Adaptador extends FragmentStateAdapter {
+    public Adaptador(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 1:
+                return new CalcVida();
+        }
+        return new Dados();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
---
+++ app/src/main/java/com/example/horiavtev/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.horiavtev;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    ViewPager2 pager;
+    TabLayout tab;
+    Adaptador adaptador;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        pager = findViewById(R.id.pager);
+        tab = findViewById(R.id.tab);
+        carregaPager();
+    }
+
+    private void carregaPager() {
+        FragmentManager manager = getSupportFragmentManager();
+        adaptador = new Adaptador(manager, getLifecycle());
+        pager.setAdapter(adaptador);
+        carregaTab();
+        pager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tab.selectTab(tab.getTabAt(position));
+            }
+        });
+    }
+
+    private void carregaTab() {
+        tab.addTab(tab.newTab().setText("Dados"));
+        tab.addTab(tab.newTab().setText("Cálculo de Vida"));
+        tab.setSelectedTabIndicatorColor(Color.parseColor("#0AD5EF"));
+        tab.setTabTextColors(Color.parseColor("#000000"), Color.parseColor("#0AD5EF"));
+        tab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
